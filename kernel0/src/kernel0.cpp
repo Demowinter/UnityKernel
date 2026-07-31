cf3aa104bdfeea8d6585b130e40488538ddd615e
TheProjectDark <v.shapoval.7@gmail.com>
2026-07-31 21:02:05 +0200

    Added memmove function to libkstd and test.  Made some outputs colorfull

--- kernel0/src/kernel0.cpp
+++ kernel0/src/kernel0.cpp
@@ -7,6 +7,7 @@
 #include <cstddef>
 #include <cpu.hpp>
 #include <console.hpp>
+#include <memory.hpp>
 
 namespace Kernel {
     constexpr uint32_t multibootMagic = 0x36D76289;
@@ -20,10 +21,29 @@ namespace Kernel {
         CPU::Info info = CPU::cpuid(0);
 
         Console::print("CPU manufacturer: ");
-        Console::write(&info.ebx, sizeof(info) - sizeof(info.eax));
+        Console::write(&info.ebx, sizeof(info) - sizeof(info.eax), 5);
         Console::newline();
 
         Console::println("Started kernel0");
-        Console::println("Welcome to UnityKernel! v0.1.0-alpha");
+        Console::print("Welcome to "); 
+        Console::println("UnityKernel v0.1.0-alpha", 5);
+        //memmove test
+        Console::println("Testing memmove...", 2);
+
+        char moveRight[] = "ABCDE";
+        memmove(moveRight + 1, moveRight, 4);
+        Console::print("Right: ");
+        Console::println(moveRight, 5);
+
+        char moveLeft[] = "ABCDE";
+        memmove(moveLeft, moveLeft + 1, 4);
+        Console::print("Left:  ");
+        Console::println(moveLeft, 5);
+
+        char source[] = "HELLO";
+        char destination[6] = {};
+        memmove(destination, source, sizeof(source));
+        Console::print("Copy:  ");
+        Console::println(destination, 5);
     }
 }
--- libkstd/src/memory.cpp
+++ libkstd/src/memory.cpp
@@ -14,5 +14,13 @@ extern "C" {
         return dest;
     }
 
-    void* memmove(void* dest, const void* src, size_t size);
+    void* memmove(void* dest, const void* src, size_t size) {
+        if (dest == src) return dest;
+        if (dest < src) {
+            for (size_t i = 0; i < size; i++) reinterpret_cast<uint8_t*>(dest)[i] = reinterpret_cast<const uint8_t*>(src)[i];
+        } else {
+            for (size_t i = size; i > 0; i--) reinterpret_cast<uint8_t*>(dest)[i - 1] = reinterpret_cast<const uint8_t*>(src)[i - 1];
+        }
+        return dest;
+    }
 }
