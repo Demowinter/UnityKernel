0eb2f2aba510cfdaff894283549d3a2a4dba4e3a
TheProjectDark <v.shapoval.7@gmail.com>
2026-07-31 19:50:52 +0200

    Deleted bloat

--- kernel0/src/kernel0.cpp
+++ kernel0/src/kernel0.cpp
@@ -22,34 +22,7 @@ namespace Kernel {
         Console::print("CPU manufacturer: ");
         Console::write(&info.ebx, sizeof(info) - sizeof(info.eax));
         Console::newline();
-
         Console::println("Started kernel0");
-
-        Console::newline();
-
-        std::variant<int, float, std::string_view> v = "Hello, Developer! This is the std::string_view in std::variant!";
-
-        Console::println("Testing std::variant...");
-
-        std::visit([](auto&& obj) -> void {
-            Console::println("Visiting std::variant...");
-
-            using T = std::decay_t<decltype(obj)>;
-
-            if constexpr (std::is_same_v<T, int>) Console::println("std::variant has type int");
-            else if constexpr (std::is_same_v<T, float>) Console::println("std::variant has type float");
-            else if constexpr (std::is_same_v<T, std::string_view>) {
-                Console::println("std::variant has type std::string_view");
-
-                Console::print("Extracted value: ");
-                Console::print(obj);
-            }
-
-        }, v);
-
-        std::string_view str2 = std::get<std::string_view>(v);
-
-        Console::newline();
-        Console::print("Hello!");
+        Console::println("Welcome to UnityKernel! v0.1.0-alpha");
     }
 }
