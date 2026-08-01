217ee41b2b70d68ef242384303a87be8e1e683a1
Nullnore <vitaliygr2301@gmail.com>
2026-08-01 21:22:57 +0200

    Make kernel32 noreturn

--- src/kernel32/kernel32.cpp
+++ src/kernel32/kernel32.cpp
@@ -6,7 +6,8 @@
 namespace Kernel {
     constexpr uint32_t multibootMagic = 0x36D76289;
 
-    extern "C" void kernel32(uint32_t mbMagic, uint32_t mbAddr) {
+    
+    extern "C" [[noreturn]] void kernel32(uint32_t mbMagic, uint32_t mbAddr) {
         Console::clear();
 
         if (mbMagic != multibootMagic) Console::println("Multiboot structure is corrupted");
@@ -24,5 +25,8 @@ namespace Kernel {
         Console::newline();
 
         Console::println("Welcome to UnityKernel! v0.1.0-alpha");
+
+        CPU::interrupts(false);
+        CPU::halt();
     }
 }
