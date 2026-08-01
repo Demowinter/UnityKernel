f83500f2a4b34052f535d8ac696c8303939e20f2
Nullnore <vitaliygr2301@gmail.com>
2026-08-01 22:43:06 +0200

    Add basic grub structures

--- include/kernel32/grub.hpp
+++ include/kernel32/grub.hpp
@@ -4,8 +4,14 @@
 namespace Kernel::GRUB {
     constexpr uint32_t multibootMagic = 0x36D76289;
 
-    struct Multiboot {
+    struct MultibootInfo {
+        uint32_t totalSize;
+        uint32_t reserved;
+    };
 
+    struct MultibootTag {
+        uint32_t type;
+        uint32_t size;
     };
 
     bool checkMultiboot(uint32_t magic);
