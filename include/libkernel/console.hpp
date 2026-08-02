b673357610825694173b22207167b0f02858f845
Nullnore <vitaliygr2301@gmail.com>
2026-08-02 04:59:22 +0200

    Add log functions

--- include/libkernel/console.hpp
+++ include/libkernel/console.hpp
@@ -12,8 +12,13 @@ namespace Kernel::Console {
     void newline();
 
     void write(std::byte byte);
-    void write(const void* str, uint32_t size, uint8_t color = 0x07);
+    void write(const void* str, size_t size, uint8_t color = 0x07);
 
     void print(std::string_view str, uint8_t color = 0x07);
     void println(std::string_view str, uint8_t color = 0x07);
+
+    void ok(std::string_view str, bool newline = true);
+    void info(std::string_view str, bool newline = true);
+    void warn(std::string_view str, bool newline = true);
+    void fail(std::string_view str, bool newline = true);
 }
--- src/libkernel/console.cpp
+++ src/libkernel/console.cpp
@@ -8,7 +8,7 @@ namespace Kernel::Console {
     static uint16_t cursor = 0;
 
     void clear() {
-        for (uint32_t i = 0; i < screenSize; i++) video[i] = 0x00;
+        for (size_t i = 0; i < screenSize; i++) video[i] = 0x00;
 
         cursor = 0;
     }
@@ -21,8 +21,8 @@ namespace Kernel::Console {
         video[cursor++] = (0x07 << 8) | static_cast<uint8_t>(byte);
     }
 
-    void write(const void* str, uint32_t size, uint8_t color) {
-        for (uint32_t i = 0; i < size; i++) video[cursor++] = (color << 8) | reinterpret_cast<const uint8_t*>(str)[i];
+    void write(const void* str, size_t size, uint8_t color) {
+        for (size_t i = 0; i < size; i++) video[cursor++] = (color << 8) | reinterpret_cast<const uint8_t*>(str)[i];
     }
 
     void print(std::string_view str, uint8_t color) {
@@ -33,5 +33,45 @@ namespace Kernel::Console {
         print(str, color);
 
         newline();
+    }
+
+    void ok(std::string_view str, bool newline) {
+        print("[  ");
+        print("OK", 0x0A);
+        print("  ] ");
+
+        print(str);
+
+        if (newline) Console::newline();
+    }
+
+    void info(std::string_view str, bool newline) {
+        print("[ ");
+        print("INFO", 0x0F);
+        print(" ] ");
+
+        print(str);
+
+        if (newline) Console::newline();
+    }
+
+    void warn(std::string_view str, bool newline) {
+        print("[ ");
+        print("WARN", 0x0E);
+        print(" ] ");
+
+        print(str);
+
+        if (newline) Console::newline();
+    }
+
+    void fail(std::string_view str, bool newline) {
+        print("[");
+        print("FAILED", 0x0C);
+        print("] ");
+
+        print(str);
+
+        if (newline) Console::newline();
     }
 }
