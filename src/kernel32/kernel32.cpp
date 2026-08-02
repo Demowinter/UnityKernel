37ae06457817b90dfc41e425c677054b49622134
Nullnore <vitaliygr2301@gmail.com>
2026-08-02 05:02:47 +0200

    Use logging instead println

--- src/kernel32/kernel32.cpp
+++ src/kernel32/kernel32.cpp
@@ -1,24 +1,30 @@
 #include <cstdint>
 #include <cstddef>
+#include <libkrt/krt.hpp>
 #include <libkernel/cpu.hpp>
+#include <libkernel/memory.hpp>
 #include <libkernel/console.hpp>
 #include <kernel32/grub.hpp>
 
 namespace Kernel {    
-    extern "C" [[noreturn]] void kernel32(uint32_t mbMagic, uint32_t mbAddr) {
+    extern "C" [[noreturn]] void kernel32(uint32_t mbMagic, GRUB::MultibootInfo* mbInfo) {
         Console::clear();
+        Console::info("Starting kernel32...");
 
-        if (!GRUB::checkMultiboot(mbMagic)) Console::println("Multiboot structure is corrupted");
-        else Console::println("Multiboot structure is OK");
+        Memory::initialize();
+        KernelRT::initialize();
+
+        if (!GRUB::checkMultiboot(mbMagic)) Console::info("Multiboot structure is corrupted");
+        else Console::info("Multiboot structure is OK");
 
         CPU::Info info = CPU::cpuid(0);
         auto binaryName = CPU::manufacturer(info);
 
-        Console::print("CPU manufacturer: ");
+        Console::info("CPU manufacturer: ", false);
         Console::write(binaryName.data(), binaryName.size() * sizeof(uint32_t), 5);
         Console::newline();
 
-        Console::println("Started kernel32!");
+        Console::ok("Started kernel32");
         
         Console::newline();
 
