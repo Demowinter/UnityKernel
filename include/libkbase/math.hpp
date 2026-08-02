186a103c888bb6fe8d4cdcdc53f3c7a64e42a306
Vladlen <v.shapoval.7@gmail.com>
2026-08-02 15:03:16 +0200

    Added isPowerOfTwo, alignUpPow2/alignDownPow2, roundUpDiv/roundDownDiv, minUint64/maxUint64, clampUint64 functions to math lib in libkbase.

--- include/libkbase/math.hpp
+++ include/libkbase/math.hpp
@@ -4,4 +4,12 @@
 extern "C" {
     uint64_t alignUp(uint64_t value, uint64_t alignment);
     uint64_t alignDown(uint64_t value, uint64_t alignment);
+    bool isPowerOfTwo(uint64_t value);
+    uint64_t alignUpPow2(uint64_t value, uint64_t alignment);
+    uint64_t alignDownPow2(uint64_t value, uint64_t alignment);
+    uint64_t roundUpDiv(uint64_t value, uint64_t divisor);
+    uint64_t roundDownDiv(uint64_t value, uint64_t divisor);
+    uint64_t minUint64(uint64_t a, uint64_t b);
+    uint64_t maxUint64(uint64_t a, uint64_t b);
+    uint64_t clampUint64(uint64_t value, uint64_t min, uint64_t max);
 }
--- src/libkbase/math.cpp
+++ src/libkbase/math.cpp
@@ -9,4 +9,36 @@ extern "C" {
     uint64_t alignDown(uint64_t value, uint64_t alignment) {
         return value - (value % alignment);
     }
+
+    bool isPowerOfTwo(uint64_t value) {
+        return value != 0 && (value & (value - 1)) == 0;
+    }
+
+    uint64_t alignUpPow2(uint64_t value, uint64_t alignment) {
+        return (value + (alignment - 1)) & ~(alignment - 1);
+    }
+
+    uint64_t alignDownPow2(uint64_t value, uint64_t alignment) {
+        return value & ~(alignment - 1);
+    }
+
+    uint64_t roundUpDiv(uint64_t value, uint64_t divisor) {
+        return (value + divisor - 1) / divisor;
+    }
+
+    uint64_t roundDownDiv(uint64_t value, uint64_t divisor) {
+        return value / divisor;
+    }
+
+    uint64_t minUint64(uint64_t a, uint64_t b) {
+        return a < b ? a : b;
+    }
+
+    uint64_t maxUint64(uint64_t a, uint64_t b) {
+        return a > b ? a : b;
+    }
+
+    uint64_t clampUint64(uint64_t value, uint64_t min, uint64_t max) {
+        return minUint64(maxUint64(value, min), max);
+    }
 }
