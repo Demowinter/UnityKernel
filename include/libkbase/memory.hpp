12c6bc16bada0df694ca5dc50ec5d290a529df76
Nullnore <vitaliygr2301@gmail.com>
2026-08-02 04:58:54 +0200

    Moved align function to math library

--- include/libkbase/memory.hpp
+++ include/libkbase/memory.hpp
@@ -6,6 +6,4 @@ extern "C" {
     void* memset(void* dest, uint8_t byte, size_t size);
     void* memcpy(void* dest, const void* src, size_t size);
     void* memmove(void* dest, const void* src, size_t size);
-
-    uintptr_t align(uintptr_t value, uint16_t alignment);
 }
