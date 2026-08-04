b4b397972fa24b17ab427afe4a457a30777eea96
Nullnore <vitaliygr2301@gmail.com>
2026-08-04 14:00:59 +0200

    Rename to exit.cpp

--- src/libkrt/cxa.cpp
+++
@@ -1,9 +0,0 @@
-using DestructorFunc = void(*)(void*);
-
-extern "C" {
-    // extern void* __dso_handle = &__dso_handle;
-
-    int __cxa_atexit(DestructorFunc func, void* object, void* dso) {
-        return 0;
-    }
-}
