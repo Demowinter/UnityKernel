83897aa6d514e41284a0c185fd2b011372376319
TheProjectDark <v.shapoval.7@gmail.com>
2026-08-10 14:11:51 +0200

    Added scrolll

--- src/libkernel/console.cpp
+++ src/libkernel/console.cpp
@@ -7,6 +7,12 @@ namespace Kernel::Console {
     static uint16_t* video = reinterpret_cast<uint16_t*>(0xB8000);
     static uint16_t cursor = 0;
 
+    void scroll() {
+        for (size_t i = columns; i < screenSize; i++) video[i - columns] = video[i];
+        for (size_t i = screenSize - columns; i < screenSize; i++) video[i] = 0x00;
+        cursor -= columns;
+    }
+
     void clear() {
         for (size_t i = 0; i < screenSize; i++) video[i] = 0x00;
 
@@ -15,24 +21,29 @@ namespace Kernel::Console {
 
     void newline() {
         if (cursor >= columns * rows) {
-            for (size_t i = columns; i < cursor + columns; i++) video[i - columns] = video[i];
-
-            cursor -= columns;
+            while (cursor >= screenSize) scroll();
         }
 
         cursor += columns - (cursor % columns);
+        if (cursor >= screenSize) {
+            while (cursor >= screenSize) scroll();
+        }
     }
 
     void write(std::byte byte, uint8_t color) {
+        if (cursor >= screenSize) {
+            while (cursor >= screenSize) scroll();
+        }
+
         video[cursor++] = (color << 8) | static_cast<uint8_t>(byte);
     }
 
     void write(const void* str, size_t size, uint8_t color) {
-        for (size_t i = 0; i < size; i++) video[cursor++] = (color << 8) | reinterpret_cast<const uint8_t*>(str)[i];
+        for (size_t i = 0; i < size; i++) write(std::byte{reinterpret_cast<const uint8_t*>(str)[i]}, color);
     }
 
     void print(std::string_view str, uint8_t color) {
-        for (char ch : str) video[cursor++] = (color << 8) | ch;
+        for (char ch : str) write(std::byte{static_cast<uint8_t>(ch)}, color);
     }
 
     void println(std::string_view str, uint8_t color) {
