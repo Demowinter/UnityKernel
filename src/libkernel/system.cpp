dcb9d7ca5abd1a592c017a1eaea8dfed8b07faf0
Nullnore <vitaliygr2301@gmail.com>
2026-08-11 03:23:10 +0200

    Change panic message

--- src/libkernel/system.cpp
+++ src/libkernel/system.cpp
@@ -6,10 +6,13 @@
 namespace Kernel::System {
     [[noreturn]] void panic(std::string_view who, std::string_view what) {
         Console::newline();
-        
-        Console::println("Kernel panic!", 0x0C);
-        Console::print(who, 0x0C);
-        Console::print(": ", 0x0C);
+        Console::println("==== KERNEL PANIC ====", 0x0C);
+        Console::newline();
+
+        Console::print("Caller: ", 0x0C);
+        Console::println(who);
+
+        Console::print("Reason: ", 0x0C);
         Console::println(what);
         
         CPU::interrupts(false);
