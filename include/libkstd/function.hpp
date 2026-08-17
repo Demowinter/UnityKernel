d338315c3f7f73db096d7ddbffa2a051b6d29c36
Nullnore <vitaliygr2301@gmail.com>
2026-08-18 00:34:53 +0200

    Add Function class to libkstd

--- include/libkstd/function.hpp
+++ include/libkstd/function.hpp
@@ -1,13 +1,25 @@
 #pragma once
+#include <memory>
+#include <type_traits>
 
 namespace KernelSTD {
-    class CallableBase {
-        virtual invoke();
+    template<typename Ret, typename... Args>
+    struct CallableBase {
+        virtual Ret invoke(Args... args) = 0;
+        virtual ~CallableBase() = default;
     };
 
-    template<typename Functor>
-    class Callable : CallableBase {
+    template<typename Functor, typename Ret, typename... Args>
+    struct Callable : CallableBase<Ret, Args...> {
+        template<typename F>
+        Callable(F func) : func(func) {}
 
+        Ret invoke(Args... args) override {
+            return func(args...);
+        }
+
+    private:
+        Functor func;
     };
 
     template<typename Signature>
@@ -15,6 +27,23 @@ namespace KernelSTD {
 
     template<typename Ret, typename... Args>
     class Function<Ret(Args...)> {
+        using CB = CallableBase<Ret, Args...>;
 
+    public:
+        Function() = default;
+
+        template<typename Functor>
+        Function(Functor obj) {
+            using CallableObject = Callable<Functor, Ret, Args...>;
+
+            functor = std::unique_ptr<CallableObject>{new CallableObject{obj}};
+        }
+
+        Ret operator()(Args... args) {
+            return functor->invoke(args...);
+        }
+
+    private:
+        std::unique_ptr<CB> functor;
     };
 }
