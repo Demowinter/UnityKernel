98a755dfa3658cc66f3d9d84e48ae2c194aa7907
Nullnore <vitaliygr2301@gmail.com>
2026-08-20 21:54:56 +0200

    Rename kernel32 directory to kernel

--- include/libplatform/api.hpp
+++ include/libplatform/api.hpp
@@ -1,5 +1,25 @@
 #pragma once
+#include <string_view>
 
 namespace Platform {
+    // Hardware Description Provider
+    enum class HDType {
+        ACPI,
+        DeviceTree,
+        Bootloader
+    };
+
     void initialize();
+
+    HDType descriptionType();
+
+    bool hasPCI();
+    bool hasUSB();
+
+    void bootInfo();
+
+    void shutdown();
+    void reboot();
+
+    // std::string_view
 }
