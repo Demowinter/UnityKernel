70e5c64a9929a08eaa406387bde650cb0c2223fe
Nullnore <vitaliygr2301@gmail.com>
2026-08-28 01:10:57 +0200

    Apply library changes

--- portable/kernel/cxx/kernel.cpp
+++ portable/kernel/cxx/kernel.cpp
@@ -3,11 +3,11 @@
 #include <cstddef>
 #include <cstdint>
 #include <libarch/api.hpp>
-#include <libkrt/krt.hpp>
-#include <libkernel/memory.hpp>
-#include <libkernel/console.hpp>
-#include <libkstd/function.hpp>
-#include <libkstd/string.hpp>
+#include <librt/runtime.hpp>
+#include <kernel/memory.hpp>
+#include <kernel/console.hpp>
+#include <libstd/function.hpp>
+#include <libstd/string.hpp>
 #include <libgrub/multiboot.hpp>
 
 namespace Kernel {
@@ -15,7 +15,7 @@ namespace Kernel {
         Console::info("Starting kernel32...");
 
         Memory::initialize();
-        KernelRT::initialize();
+        CXXRuntime::initialize();
 
         if (!GRUB::checkMultiboot(mbMagic)) Console::info("Multiboot structure is corrupted");
         else Console::info("Multiboot structure is OK");
@@ -30,12 +30,12 @@ namespace Kernel {
         Console::print("Welcome to ");
         Console::println("UnityKernel! v0.1.0-alpha", 5);
 
-        KernelSTD::String str = "Hello, World!";
-        KernelSTD::Function<void()> func = [str](){ Console::ok(str); };
+        STDLib::String str = "Hello, World!";
+        STDLib::Function<void()> func = [str](){ Console::ok(str); };
 
         func();
 
-        KernelRT::finalize();
+        CXXRuntime::finalize();
 
         Arch::Interrupt::disable();
         Arch::CPU::halt();
