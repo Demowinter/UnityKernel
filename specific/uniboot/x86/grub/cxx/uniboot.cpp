fc4820808d834bf547b8784c18fdfaf87461bba4
Nullnore <vitaliygr2301@gmail.com>
2026-08-28 01:04:26 +0200

    Add grub multiboot checking

--- specific/uniboot/x86/grub/cxx/uniboot.cpp
+++ specific/uniboot/x86/grub/cxx/uniboot.cpp
@@ -1,11 +1,20 @@
 #include <libgrub/multiboot.hpp>
-#include <libkernel/console.hpp>
 #include <libarch/api.hpp>
-#include <libuniboot/protocol.hpp>
+#include <uniboot/protocol.hpp>
+#include <uniboot/console.hpp>
 
 namespace UniBoot {
-    extern "C" [[noreturn]] void unibootMain() {
-        Kernel::Console::info("Hello from UniBoot!");
+    extern "C" [[noreturn]] void unibootMain(uint32_t mbMagic, GRUB::MultibootInfo* mbInfo) {
+        Console::info("Hello from UniBoot!");
+
+        if (!GRUB::checkMultiboot(mbMagic)) {
+            Console::fail("Multiboot structure is corrupted");
+
+            Arch::Interrupt::disable();
+            Arch::CPU::halt();
+        }
+
+        Console::ok("Multiboot structure is OK");
 
         Arch::Interrupt::disable();
         Arch::CPU::halt();
