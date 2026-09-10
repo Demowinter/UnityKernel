5e76d97c506431e3ec1a05f0dc61642d7da6ddfd
Nullnore <vitaliygr2301@gmail.com>
2026-09-10 16:10:36 +0300

    Move int <-> string conversion functions to string.hpp/string.cpp

--- include/libbase/string.hpp
+++ include/libbase/string.hpp
@@ -5,4 +5,10 @@
 extern "C" {
     size_t strlen(const char* str);
     int32_t strcmp(const char* str1, const char* str2);
+
+    char* itoa(int64_t value, char* buffer, int base);
+    char* utoa(uint64_t value, char* buffer, int base);
+    char* ftoa(float value, char* buffer, int precision);
+    char* ltoa(int64_t value, char* buffer, int base);
+    char* ultoa(uint64_t value, char* buffer, int base);
 }
