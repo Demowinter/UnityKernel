bbebf243c9a15ee0980c6cec96b98e4615f667c3
Nullnore <vitaliygr2301@gmail.com>
2026-09-17 15:41:00 +0300

    Implement memory freeze functionality and enhance getFreeMemoryRegion method

--- include/unityboot/memory.hpp
+++ include/unityboot/memory.hpp
@@ -5,8 +5,17 @@ namespace UnityBoot::Memory {
     constexpr size_t heapAlignment = 16;
     constexpr size_t minBlockSize = 32;
 
+    struct MemoryRegion {
+        uintptr_t start;
+        uintptr_t end;
+    };
+
     void initialize();
 
     void* allocate(size_t size);
     void deallocate(void* ptr);
+
+    void freeze();
+    
+    MemoryRegion getFreeMemoryRegion();
 }
--- specific/unityboot/x86/grub/cxx/memory.cpp
+++ specific/unityboot/x86/grub/cxx/memory.cpp
@@ -9,6 +9,7 @@ namespace UnityBoot::Memory {
     }
 
     static LLTools::FirstFitAllocator heapAlloc{};
+    static bool freezeFlag = false;
 
     void initialize() {
         uintptr_t heapStartAddr = reinterpret_cast<uintptr_t>(__heap_start);
@@ -18,10 +19,20 @@ namespace UnityBoot::Memory {
     }
 
     void* allocate(size_t size) {
-        return heapAlloc.allocate(size);
+        return (freezeFlag) ? nullptr : heapAlloc.allocate(size);
     }
 
     void deallocate(void* ptr) {
-        heapAlloc.deallocate(static_cast<uint8_t*>(ptr));
+        if (!freezeFlag) heapAlloc.deallocate(static_cast<uint8_t*>(ptr));
+    }
+
+    void freeze() {
+        freezeFlag = true;
+    }
+
+    MemoryRegion getFreeMemoryRegion() {
+        LLTools::MemoryBlockHeader* mbh = heapAlloc.getLastBlock();
+
+        return MemoryRegion{reinterpret_cast<uintptr_t>(mbh), reinterpret_cast<uintptr_t>(mbh) + mbh->size + sizeof(LLTools::MemoryBlockHeader)};
     }
 }
